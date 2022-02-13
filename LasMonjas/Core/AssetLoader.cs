fefd76d94cc241f3e7be8defe65811101a60ad57
Allul <forte_crisx@hotmail.com>
2022-02-13 17:32:42 +0100

    Add files via upload

---
+++ LasMonjas/Core/AssetLoader.cs
@@ -0,0 +1,109 @@
+﻿using Reactor.Extensions;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LasMonjas.Core
+{
+    public static class AssetLoader
+    {
+        private static readonly Assembly allulCustomBundle = Assembly.GetExecutingAssembly();
+        private static readonly Assembly allulCustomLobby = Assembly.GetExecutingAssembly();
+        private static readonly Assembly allulCustomMusic = Assembly.GetExecutingAssembly();
+        private static readonly Assembly allulCustomMap = Assembly.GetExecutingAssembly();
+
+        public static void LoadAssets() {
+
+            // Custom Bundle Assets
+            var resourceStreamBundle = allulCustomBundle.GetManifestResourceStream("LasMonjas.Images.MyAssets.allulcustombundle");
+            var assetBundleBundle = AssetBundle.LoadFromMemory(resourceStreamBundle.ReadFully());
+
+            CustomMain.customAssets.bombermanBomb = assetBundleBundle.LoadAsset<GameObject>("Bomb.prefab").DontUnload();
+            CustomMain.customAssets.bombermanPlaceBombClip = assetBundleBundle.LoadAsset<AudioClip>("bombermanPlaceBombClip_Switch2.ogg").DontUnload();
+            CustomMain.customAssets.bombermanBombClip = assetBundleBundle.LoadAsset<AudioClip>("bombermanBombClip_Explosion2.ogg").DontUnload();
+            CustomMain.customAssets.renegadeRecruitMinionClip = assetBundleBundle.LoadAsset<AudioClip>("renegadeRecruitMinionClip_Sword4.ogg").DontUnload();
+            CustomMain.customAssets.trapperMine = assetBundleBundle.LoadAsset<GameObject>("mine.prefab").DontUnload();
+            CustomMain.customAssets.trapperStepMineClip = assetBundleBundle.LoadAsset<AudioClip>("trapperStepMineClip_Explosion1.ogg").DontUnload();
+            CustomMain.customAssets.trapperTrap = assetBundleBundle.LoadAsset<GameObject>("trap.prefab").DontUnload();
+            CustomMain.customAssets.trapperStepTrapClip = assetBundleBundle.LoadAsset<AudioClip>("trapperStepTrapClip_Slash10.ogg").DontUnload();
+            CustomMain.customAssets.yinyangerYinyang = assetBundleBundle.LoadAsset<GameObject>("yinyang.prefab").DontUnload();
+            CustomMain.customAssets.yinyangerYinyangClip = assetBundleBundle.LoadAsset<AudioClip>("yinyangerYinyangClip_Skill3.ogg").DontUnload();
+            CustomMain.customAssets.yinyangerYinyangColisionClip = assetBundleBundle.LoadAsset<AudioClip>("yinyangerYinyangColisionClip_Bell1.ogg").DontUnload();
+            CustomMain.customAssets.challengerDuelArena = assetBundleBundle.LoadAsset<GameObject>("challenger_Arena.prefab").DontUnload();
+            CustomMain.customAssets.challengerRock = assetBundleBundle.LoadAsset<GameObject>("rockshow.prefab").DontUnload();
+            CustomMain.customAssets.challengerPaper = assetBundleBundle.LoadAsset<GameObject>("papershow.prefab").DontUnload();
+            CustomMain.customAssets.challengerScissors = assetBundleBundle.LoadAsset<GameObject>("scissorsshow.prefab").DontUnload();
+            CustomMain.customAssets.challengerDuelKillClip = assetBundleBundle.LoadAsset<AudioClip>("challengerDuelKillClip_Blow8.ogg").DontUnload();
+            CustomMain.customAssets.roleThiefStealRole = assetBundleBundle.LoadAsset<AudioClip>("roleThiefStealRole_Miss.ogg").DontUnload();
+            CustomMain.customAssets.pyromaniacIgniteClip = assetBundleBundle.LoadAsset<AudioClip>("pyromaniacIgniteClip_Fire2.ogg").DontUnload();
+            CustomMain.customAssets.treasureHunterTreasure = assetBundleBundle.LoadAsset<GameObject>("treasure.prefab").DontUnload();
+            CustomMain.customAssets.treasureHunterPlaceTreasure = assetBundleBundle.LoadAsset<AudioClip>("treasureHunterPlaceTreasure_Flash2.ogg").DontUnload();
+            CustomMain.customAssets.treasureHunterCollectTreasure = assetBundleBundle.LoadAsset<AudioClip>("treasureHunterCollectTreasure_Item3.ogg").DontUnload();
+            CustomMain.customAssets.devourerDingClip = assetBundleBundle.LoadAsset<AudioClip>("devourerDingClip_Bell3.ogg").DontUnload();
+            CustomMain.customAssets.devourerDevourClip = assetBundleBundle.LoadAsset<AudioClip>("devourerDevourClip_Slash1.ogg").DontUnload();
+            CustomMain.customAssets.timeTravelerTimeReverseClip = assetBundleBundle.LoadAsset<AudioClip>("timeTravelerTimeReverseClip_Magic3.ogg").DontUnload();
+            CustomMain.customAssets.squireShieldClip = assetBundleBundle.LoadAsset<AudioClip>("squireShieldClip_Parry.ogg").DontUnload();
+            CustomMain.customAssets.fortuneTellerRevealClip = assetBundleBundle.LoadAsset<AudioClip>("fortuneTellerRevealClip_Magic2.ogg").DontUnload(); 
+            CustomMain.customAssets.spiritualistRevive = assetBundleBundle.LoadAsset<AudioClip>("spiritualistRevive_Recovery.ogg").DontUnload();
+            CustomMain.customAssets.performerDio = assetBundleBundle.LoadAsset<GameObject>("dio.prefab").DontUnload();
+            CustomMain.customAssets.jinxQuack = assetBundleBundle.LoadAsset<AudioClip>("jinxQuack_quack.mp3").DontUnload();
+
+            // Capture the flag
+            CustomMain.customAssets.redflag = assetBundleBundle.LoadAsset<GameObject>("redFlag.prefab").DontUnload();
+            CustomMain.customAssets.redflagbase = assetBundleBundle.LoadAsset<GameObject>("redFlagBase.prefab").DontUnload();
+            CustomMain.customAssets.blueflag = assetBundleBundle.LoadAsset<GameObject>("blueFlag.prefab").DontUnload();
+            CustomMain.customAssets.blueflagbase = assetBundleBundle.LoadAsset<GameObject>("blueFlagBase.prefab").DontUnload();
+
+            // Police and Thief
+            CustomMain.customAssets.cell = assetBundleBundle.LoadAsset<GameObject>("Cell.prefab").DontUnload();
+            CustomMain.customAssets.jewelbutton = assetBundleBundle.LoadAsset<GameObject>("deliverjewel_floor.prefab").DontUnload();
+            CustomMain.customAssets.freethiefbutton = assetBundleBundle.LoadAsset<GameObject>("cell_button.prefab").DontUnload();
+            CustomMain.customAssets.jeweldiamond = assetBundleBundle.LoadAsset<GameObject>("jewel_diamond.prefab").DontUnload();
+            CustomMain.customAssets.jewelruby = assetBundleBundle.LoadAsset<GameObject>("jewel_ruby.prefab").DontUnload();
+            CustomMain.customAssets.thiefspaceship = assetBundleBundle.LoadAsset<GameObject>("thief_spaceship.prefab").DontUnload();
+            CustomMain.customAssets.thiefspaceshiphatch = assetBundleBundle.LoadAsset<GameObject>("thief_spaceship_hatch.prefab").DontUnload();
+
+            // Custom Lobby Assets
+            var resourceStreamLobby = allulCustomLobby.GetManifestResourceStream("LasMonjas.Images.MyAssets.allulcustomlobby");
+            var assetBundleLobby = AssetBundle.LoadFromMemory(resourceStreamLobby.ReadFully());
+
+            CustomMain.customAssets.customLobby = assetBundleLobby.LoadAsset<GameObject>("allul_customLobby.prefab").DontDestroy();
+
+            // Custom Map Assets
+            var resourceStreamMap = allulCustomMap.GetManifestResourceStream("LasMonjas.Images.MyAssets.allulcustommap");
+            var assetBundleMap = AssetBundle.LoadFromMemory(resourceStreamMap.ReadFully());
+
+            CustomMain.customAssets.customMap = assetBundleMap.LoadAsset<GameObject>("HalconUI.prefab").DontUnload();
+            CustomMain.customAssets.customMinimap = assetBundleMap.LoadAsset<GameObject>("Minimap.prefab").DontUnload();
+            CustomMain.customAssets.customComms = assetBundleMap.LoadAsset<GameObject>("new_comms.prefab").DontUnload();
+
+            // Custom Music Assets
+            var resourceStream = allulCustomMusic.GetManifestResourceStream("LasMonjas.Images.MyAssets.allulcustommusic");
+            var assetBundleMusic = AssetBundle.LoadFromMemory(resourceStream.ReadFully());
+
+            CustomMain.customAssets.bombermanBombMusic = assetBundleMusic.LoadAsset<AudioClip>("BombermanTheme_Run Amok.mp3").DontUnload();
+            CustomMain.customAssets.challengerDuelMusic = assetBundleMusic.LoadAsset<AudioClip>("ChallengerTheme_FutureGladiator.mp3").DontUnload();
+            CustomMain.customAssets.performerMusic = assetBundleMusic.LoadAsset<AudioClip>("PerformerTheme_Spazzmatica Polka.mp3").DontUnload();
+            CustomMain.customAssets.captureTheFlagMusic = assetBundleMusic.LoadAsset<AudioClip>("CaptureTheFlagMusic_BeachfrontCelebration.mp3").DontUnload();
+            CustomMain.customAssets.policeAndThiefMusic = assetBundleMusic.LoadAsset<AudioClip>("PoliceAndThief_Unity.mp3").DontUnload();
+            CustomMain.customAssets.lobbyMusic = assetBundleMusic.LoadAsset<AudioClip>("Lobby_Hyperfun.mp3").DontUnload();
+            CustomMain.customAssets.tasksCalmMusic = assetBundleMusic.LoadAsset<AudioClip>("TasksCalm_Sneaky Adventure.mp3").DontUnload();
+            CustomMain.customAssets.tasksCoreMusic = assetBundleMusic.LoadAsset<AudioClip>("TasksCore_Investigations.mp3").DontUnload();
+            CustomMain.customAssets.tasksFinalMusic = assetBundleMusic.LoadAsset<AudioClip>("TasksFinal_Hidden Agenda.mp3").DontUnload(); 
+            CustomMain.customAssets.meetingCalmMusic = assetBundleMusic.LoadAsset<AudioClip>("MeetingCalm_Fluffing a Duck.mp3").DontUnload();
+            CustomMain.customAssets.meetingCoreMusic = assetBundleMusic.LoadAsset<AudioClip>("MeetingCore_Local Forecast.mp3").DontUnload();
+            CustomMain.customAssets.meetingFinalMusic = assetBundleMusic.LoadAsset<AudioClip>("MeetingFinal_Heavy Interlude.mp3").DontUnload();            
+            CustomMain.customAssets.winCrewmatesMusic = assetBundleMusic.LoadAsset<AudioClip>("WinCremates_Take a Chance.mp3").DontUnload();
+            CustomMain.customAssets.winImpostorsMusic = assetBundleMusic.LoadAsset<AudioClip>("WinImpostors_Who Likes to Party.mp3").DontUnload();
+            CustomMain.customAssets.winNeutralsMusic = assetBundleMusic.LoadAsset<AudioClip>("WinNeutrals_Mistake the Getaway.mp3").DontUnload();
+            CustomMain.customAssets.winRebelsMusic = assetBundleMusic.LoadAsset<AudioClip>("WinRebels_Danse Macabre - Low Strings Finale.mp3").DontUnload();
+
+            assetBundleBundle.Unload(false);
+            assetBundleLobby.Unload(false);
+            assetBundleMap.Unload(false);
+            assetBundleMusic.Unload(false);
+
+        }
+    }
+}
---
+++ LasMonjas/Core/CustomMessage.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace LasMonjas.Core {
+
+    public class CustomMessage
+    {
+
+        private TMPro.TMP_Text text;
+        private static List<CustomMessage> customMessages = new List<CustomMessage>();
+        private int localBombNumber = 0;
+
+        public CustomMessage(string message, float duration, int bombNumber, float localPosition, int whichmessage) {
+            RoomTracker roomTracker = HudManager.Instance?.roomTracker;
+            localBombNumber = bombNumber;
+            if (roomTracker != null) {
+                GameObject gameObject = UnityEngine.Object.Instantiate(roomTracker.gameObject);
+
+                gameObject.transform.SetParent(HudManager.Instance.transform);
+                UnityEngine.Object.DestroyImmediate(gameObject.GetComponent<RoomTracker>());
+                text = gameObject.GetComponent<TMPro.TMP_Text>();
+                text.text = message;
+
+                gameObject.transform.localPosition = new Vector3(0, localPosition, gameObject.transform.localPosition.z);
+                customMessages.Add(this);
+
+                HudManager.Instance.StartCoroutine(Effects.Lerp(duration, new Action<float>((p) => {
+                    bool even = ((int)(p * duration / 0.25f)) % 2 == 0;
+                    switch (whichmessage) {
+                        case 1:
+                            // Bomberman bomb warning
+                            if (Bomberman.activeBomb == true && localBombNumber == Bomberman.currentBombNumber) {
+                                string prefix = (even ? "<color=#FCBA03FF>" : "<color=#FF0000FF>");
+                                text.text = prefix + message + Bomberman.bombTimer.ToString("F0") + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 2:
+                            // Challenger duel timer
+                            if (Challenger.isDueling == true && Challenger.duelDuration >= 0 && Challenger.onlyOneFinishDuel) {
+                                string prefix = (even ? "<color=#FCBA03FF>" : "<color=#4F7D00FF>");
+                                text.text = prefix + message + Challenger.duelDuration.ToString("F0") + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 3:
+                            // Capture the flag timer
+                            if (CaptureTheFlag.captureTheFlagMode && CaptureTheFlag.matchDuration >= 0) {
+                                string prefix = ("<color=#FF8000FF>");
+                                text.text = prefix + message + CaptureTheFlag.matchDuration.ToString("F0") + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 4:
+                            // Capture the flag which team scores
+                            if (CaptureTheFlag.captureTheFlagMode && CaptureTheFlag.matchDuration >= 0) {
+                                string prefix = ("<color=#FF8000FF>");
+                                text.text = prefix + message + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 5:
+                            // Capture the flag point counter
+                            if (CaptureTheFlag.captureTheFlagMode && CaptureTheFlag.matchDuration >= 0) {
+                                string prefix = ("<color=#FF8000FF>");
+                                text.text = prefix + CaptureTheFlag.flagpointCounter + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 6:
+                            // Polic and thiefs timer
+                            if (PoliceAndThief.policeAndThiefMode && PoliceAndThief.matchDuration >= 0) {
+                                string prefix = ("<color=#FF8000FF>");
+                                text.text = prefix + message + PoliceAndThief.matchDuration.ToString("F0") + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 7:
+                            // Police and thiefs warnings
+                            if (PoliceAndThief.policeAndThiefMode && PoliceAndThief.matchDuration >= 0) {
+                                string prefix = ("<color=#FF8000FF>");
+                                text.text = prefix + message + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 8:
+                            // Police and thiefs point counter
+                            if (PoliceAndThief.policeAndThiefMode && PoliceAndThief.matchDuration >= 0) {
+                                string prefix = ("<color=#FF8000FF>");
+                                text.text = prefix + PoliceAndThief.thiefpointCounter + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                        case 9:
+                            // Ilusionist light out ability timer for other impostors
+                            if (Ilusionist.lightsOutTimer > 0) {
+                                string prefix = (even ? "<color=#FCBA03FF>" : "<color=#FF0000FF>");
+                                text.text = prefix + message + Ilusionist.lightsOutTimer.ToString("F0") + "</color>";
+                            }
+                            else {
+                                text.text = "";
+                            }
+                            break;
+                    }
+                    if (text != null) text.color = even ? Color.yellow : Color.red;
+                    if (p == 1f && text != null && text.gameObject != null) {
+                        UnityEngine.Object.Destroy(text.gameObject);
+                        customMessages.Remove(this);
+                    }
+                })));
+            }
+        }
+    }
+}
